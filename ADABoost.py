e801db83a11bf6fb19ca768a838df87b77a4b018
Daniel Campos <danielcampos@dcc.ufmg.br>
2022-07-06 09:37:17 -0300

    feature: Final prediction considering trees weights

--- ADABoost.py
+++ ADABoost.py
@@ -1,5 +1,6 @@
 from sklearn import tree
 import pandas as pd
+import numpy as np
 
 class ADABoost():
     
@@ -7,7 +8,8 @@ class ADABoost():
         self._max_n_trees = num_trees
         self._max_tree_height = tree_h
         self._random_seed = random_seed
-        self._tree = None
+        self._trees = list()
+        self._trees_alphas = list()
     
     @property
     def max_n_trees(self):
@@ -44,14 +46,56 @@ class ADABoost():
         self._max_tree_height = new_max
     
     def fit(self, dataframe:pd.DataFrame, target_col:str):
-        my_tree = tree.DecisionTreeClassifier(max_depth=self._max_tree_height, random_state=self._random_seed)
-        
+        sample_weights = self._get_starting_weights(len(dataframe))
+
         x = dataframe.drop(target_col, axis=1)
         y = dataframe[target_col]
-        my_tree.fit(x, y)
 
-        self._tree = my_tree
+        tree_count = 0
+        while tree_count < self._max_n_trees:
+            curr_tree = tree.DecisionTreeClassifier(max_depth=self._max_tree_height, random_state=self._random_seed)
+            curr_tree.fit(x, y, sample_weight=sample_weights)
+
+            #Pegar onde essa árvore errou
+
+            #Atualizar os pesos
+
+            tree_count += 1
+            self._trees.append(curr_tree)
+            self._trees_alphas.append(1)
+    
+    def _get_starting_weights(self, data_len:int) ->np.array:
+        return np.full((data_len), 1/data_len)
 
     def predict(self, dataframe:pd.DataFrame, target_col:str) -> list:
         x = dataframe.drop(target_col, axis=1)
-        return self._tree.predict(x)
+        return self._predict_from_trees(x)
+    
+    def _predict_from_trees(self, x:pd.DataFrame) -> int:
+        
+        results = [tree.predict(x) for tree in self._trees]
+
+        final_results = list()
+        num_inputs = len(results[0])
+        for curr_input_prediction_idx in range(num_inputs):
+            predictions_for_curr_input = [preds[curr_input_prediction_idx] for preds in results]
+            final_results.append(self._weighted_most_frequent(predictions_for_curr_input))
+
+        return final_results
+    
+    def _weighted_most_frequent(self, numbers:list) -> float:
+
+        weight_per_result = dict()
+
+        biggest_weight = -1
+        biggest_weight_result = None
+        for result_idx in range(len(numbers)):
+            curr_result = numbers[result_idx]
+            weight_per_result[curr_result] = weight_per_result.get(curr_result, 0) + self._trees_alphas[result_idx]
+            
+            curr_result_weight = weight_per_result[curr_result]
+            if curr_result_weight > biggest_weight:
+                biggest_weight = curr_result_weight
+                biggest_weight_result = curr_result
+
+        return biggest_weight_result
